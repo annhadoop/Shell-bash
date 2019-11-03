212b64c978d1974118f8b0956a4d79584dce3f2e
annhadoop <annhadoop@gmail.com>
2019-11-03 22:44:58 +0530

    Update stackoverflow_spider.py

--- python/stackoverflow_spider.py
+++ python/stackoverflow_spider.py
@@ -6,6 +6,7 @@ def parse(self, response): # for each request this generator yields, its respons
             for href in response.css('.question-summary h3 a::attr(href)'): # do some scraping stuff using css selectors to find question urls
                         full_url = response.urljoin(href.extract())
                         yield scrapy.Request(full_url, callback=self.parse_question)
+                        
 def parse_question(self, response): yield {
             'title': response.css('h1 a::text').extract_first(),
             'votes': response.css('.question .vote-count-post::text').extract_first(),
